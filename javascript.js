96a2faec199292fd2c13381be7fed5edfaeb6a57
Juan Arvelo <arveloarias@gmail.com>
2022-06-26 08:21:03 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    element.classList.toggle("li-dark");
+    element.classList.toggle("botton-dark");
+ }
